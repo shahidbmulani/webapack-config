e4d88c5f34b2ecb62be82943866b653be1742ee9
Shahid Mulani <shahidm@techspawn.co>
2022-01-07 12:39:12 +0530

    add css, postcss, sass, hot reloading, browserslist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.css';
+
 const elevenShield = {
     letherStrips: 2,
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 let mode = "development";
+let target = "web";
 
 if ( process.env.NODE_ENV === "production" )
 {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
+    target: target,
     devtool: "source-map",
     module: {
         rules: [
             {
-                test: /\.js/,
+                test: /\.(s[ac]|c)ss$/i,
+                use: [ MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader" ]
+            },
+            {
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
@@ -19,7 +27,9 @@ module.exports = {
             },
         ],
     },
+    plugins: [ new MiniCssExtractPlugin() ],
     devServer: {
         contentBase: "./dist",
+        hot: true,
     },
 };
